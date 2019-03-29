866b0fc96fe76d76e2281ac1ab9ef21cf5f836cc
Codey Pinero <codey.pinero@gmail.com>
2019-03-29 16:34:00 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,8 @@ ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
   arr.sort_by do |a|
-    a.split("").map do |char| ESPERANTO_ALPHABET.index(char)
-    end 
+    a.split("").map do |char|
+      ESPERANTO_ALPHABET.index(char)
+    end
   end
 end
